63196d908a0ee53a9a4251ffcc5e0bd4f369760a
bowen-wu <13793248162@163.com>
2018-01-05 14:00:38 +0800

    增加导航栏效果

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 window.onload = function(){
+
+    websiteLoading();
+    window.onscroll = function(){
+        var topNavBar = document.getElementById("topNavBar");
+        var scrollHeight = window.scrollY;
+        if(scrollHeight > 50){
+            topNavBar.classList.add('active');
+        }else{
+            topNavBar.classList.remove('active');
+        }
+    }
+
     var portfolioAll = document.getElementById("portfolioAll");
     var portfolioFrame = document.getElementById("portfolioFrame");
     var portfolioProtogenesis = document.getElementById("portfolioProtogenesis");
@@ -11,5 +23,13 @@ window.onload = function(){
     }
     portfolioProtogenesis.onclick = function(){
         portfolioBar.className = "sliderBar-inner sliderBarStateThird"
+    }
+
+    /********工具函数************/
+    function websiteLoading() {
+        var loading = document.getElementById("loading");
+        setTimeout(function () {
+            loading.classList.add('active');
+        })
     }
 }
