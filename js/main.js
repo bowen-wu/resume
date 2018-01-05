8acbc4cd3b4a793e25a74ab9a951b64e545ef7ea
bowen-wu <13793248162@163.com>
2018-01-05 18:56:36 +0800

    增加导航条鼠标事件和点击事件，页面滚动事件

--- js/main.js
+++ js/main.js
@@ -1,26 +1,63 @@
 window.onload = function(){
 
     websiteLoading();
-    var topNavBar = document.getElementById("topNavBar");
-    window.onscroll = function(){
-        var scrollHeight = window.scrollY;
-        if(scrollHeight > 50){
-            topNavBar.classList.add('active');
-        }else{
-            topNavBar.classList.remove('active');
+    let topNavBar = document.getElementById("topNavBar");
+    let topNavBarLis = document.querySelectorAll('.topNavBar nav>ul>li');
+    let topNavBarATag = document. querySelectorAll('.topNavBar nav>ul>li>a');
+    for(let i=0, len=topNavBarLis.length; i<len; i++){
+        topNavBarLis[i].onmouseenter = function(event){
+            event.currentTarget.classList.add('mouseEventActive');
+        }
+        topNavBarLis[i].onmouseleave = function(event){
+            event.currentTarget.classList.remove('mouseEventActive');
+        }
+        topNavBarLis[i].onclick = function(event){
+            event.preventDefault();
+            let siblings = event.currentTarget.parentNode.childNodes;
+            let allEleChilds = filterElementNode(siblings,'multi');
+            for(let i=0,len=allEleChilds.length;i<len;i++){
+                allEleChilds[i].classList.remove("clickEventActive");
+            }
+            event.currentTarget.classList.add("clickEventActive");
+            let topNavBarATagClick = filterElementNode(event.currentTarget.childNodes,'single');
+            let topNavBarATagClickHref = topNavBarATagClick.getAttribute('href');
+            let mainSection = document.querySelector(topNavBarATagClickHref);
+            if (mainSection !== null) {
+                let mainSectionOffsetTop = mainSection.offsetTop;
+                window.scrollTo(0, mainSectionOffsetTop - 100);
+            }else {
+                return;
+            }
         }
     }
-    let topNavBarLi = document.querySelectorAll('.topNavBar nav>ul>li');
-    let topNavBarUl = document.querySelector('.topNavBar nav>ul');
-    topNavBarUl.onclick = function(event){
-        event.preventDefault();
-        let triggerEle = event.target;
-        let getHref = triggerEle.getAttribute('href');
-        let triggerEleTarget = document.querySelector(getHref);
-        let triggerEleTargetOffsetTop = triggerEleTarget.offsetTop;
-        window.scrollTo(0,triggerEleTargetOffsetTop-80);
+    
+    window.onscroll = function () {
+        var scrollHeight = window.scrollY;
+        if (scrollHeight > 50) {
+            topNavBar.classList.add('active');
+        } else {
+            topNavBar.classList.remove('active');
+        }
+        let scrollEles = document.getElementsByClassName('scrollClass');
+        for(let i=0,len=scrollEles.length;i<len;i++){
+            let currentEle = scrollEles[i];
+            let currentEleOffsetTop = currentEle.offsetTop;
+            let currentEleHeight = currentEle.offsetHeight;
+            if(scrollHeight > currentEleOffsetTop-100 || scrollHeight > currentEleOffsetTop + currentEleHeight-100){
+                for(let i=0,len=topNavBarATag.length;i<len;i++){
+                    let topNavBarATagHrefScroll = topNavBarATag[i].getAttribute('href');
+                    if('#' + currentEle.id === topNavBarATagHrefScroll){
+                        for(let i=0,len=topNavBarLis.length;i<len;i++){
+                            topNavBarLis[i].classList.remove('scrollEventActive');
+                        }
+                        topNavBarATag[i].parentNode.classList.add('scrollEventActive');
+                    }
+                }
+            }else{
+                
+            }
+        }
     }
-
 
 
 
@@ -46,6 +83,27 @@ window.onload = function(){
         var loading = document.getElementById("loading");
         setTimeout(function () {
             loading.classList.add('active');
-        })
+        },2500)
+    }
+    // 筛选单个元素节点
+    function filterElementNode(nodes,num){
+        if(num === 'single'){
+            var node=null;
+            for(let i=0, len=nodes.length;i<len;i++){
+                if(nodes[i].nodeType === 1){
+                    node = nodes[i];
+                    break;
+                }
+            }
+        }
+        if(num === 'multi'){
+            var node=[];
+            for(let i=0, len=nodes.length;i<len;i++){
+                if(nodes[i].nodeType === 1){
+                    node.push(nodes[i]);
+                }
+            }
+        }
+        return node;
     }
 }
