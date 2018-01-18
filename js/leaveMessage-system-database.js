d313fc9c6d75d98c228d85596c88ef85dbd2fd11
bowen-wu <13793248162@163.com>
2018-01-18 19:20:19 +0800

    完成需求，并整理代码到 V + C

--- js/leaveMessage-system-database.js
+++ js/leaveMessage-system-database.js
@@ -1,113 +1,105 @@
 !function () {
     let view = document.querySelector('section.leaveMessage')
-
-
-    var APP_ID = 'sVayEmmvvuiy4NFwyNWSazU3-gzGzoHsz';
-    var APP_KEY = 'fIzNhLcRHKDNloQJztPifTVe';
-
-    AV.init({
-        appId: APP_ID,
-        appKey: APP_KEY
-    });
-
-    var messages = new AV.Query('Message');
-    messages.find().then(function (messages) {
-        let countMessage = document.querySelector('#countMessage')
-        countMessage.textContent = messages.length
-        messages.forEach(function (message) {
-            let data = message.set('status', 1)
-            createLi(data)
-        });
-    })
-
-    let form = document.querySelector('section.leaveMessage form')
-    submitEvent()
-    function submitEvent() {
-        form.addEventListener('submit', (event) => {
-            event.preventDefault()
-            let username = document.querySelector('section.leaveMessage form input[name=username]').value
-            let content = document.querySelector('section.leaveMessage form input[name=content]').value
-            var Message = AV.Object.extend('Message');
-            var message = new Message();
-            message.save({
-                username: username,
-                content: content
-            }).then(function (object) {
-                let date = object.set('status', 1)
-                let countMessage = document.querySelector('#countMessage')
-                countMessage.textContent = parseInt(countMessage.textContent, 10) + 1
-                createLi(object)
-                document.querySelector('section.leaveMessage form input[name=username]').value = ''
-                document.querySelector('section.leaveMessage form input[name=content]').value = ''
+    let controller = {
+        view: null,
+        ol: null,
+        form: null,
+        countMessage: null,
+        content: null,
+        submit: null,
+        username: null,
+        init: function () {
+            this.view = view
+            this.ol = view.querySelector('#messageList')
+            this.form = view.querySelector('section.leaveMessage form')
+            this.countMessage = view.querySelector('#countMessage')
+            this.submit = view.querySelector('#leaveMessageSubmit')
+            this.content = view.querySelector('#leaveMessageContent')
+            this.username = view.querySelector('#leaveMessageUsername')
+            this.initAV()
+            this.readingDate() // this.readingDate.call(this)
+            this.bindEvents()  //this.bindEvents.call(this)
+        },
+        initAV: function () {
+            var APP_ID = 'sVayEmmvvuiy4NFwyNWSazU3-gzGzoHsz';
+            var APP_KEY = 'fIzNhLcRHKDNloQJztPifTVe';
+            AV.init({ appId: APP_ID, appKey: APP_KEY })
+        },
+        readingDate: function () {
+            let messages = new AV.Query('Message');
+            messages.find().then((messages) => {
+                this.countMessage.textContent = messages.length
+                messages.forEach((message) => {
+                    // let data = message.set('status', 1)
+                    this.createLi(message)
+                })
             })
-        })
+        },
+        bindEvents: function () {
+            this.form.addEventListener('submit', (event) => {
+                event.preventDefault()
+                this.submit.classList.remove('active')
+                let username = view.querySelector('form input[name=username]').value
+                let content = view.querySelector('form input[name=content]').value
+                var Message = AV.Object.extend('Message');
+                var message = new Message()
+                message.save({
+                    username: username,
+                    content: content
+                }).then((object) => {
+                    let date = object.set('status', 1)
+                    let countMessage = document.querySelector('#countMessage')
+                    countMessage.textContent = parseInt(countMessage.textContent, 10) + 1
+                    this.createLi(date)
+                    document.querySelector('#leaveMessageUsername').value = ''
+                    document.querySelector('#leaveMessageContent').value = ''
+                })
+            })
+            this.content.addEventListener('input', () => {
+                this.submitVerification()
+            })
+            this.username.addEventListener('input', () => {
+                this.submitVerification()
+            })
+        },
+        submitVerification: function(){
+            if (this.content.value !== '' && this.username.value !== '') {
+                this.submit['disabled'] = false
+                this.submit.classList.add('active')
+            } else {
+                this.submit['disabled'] = true
+                this.submit.classList.remove('active')
+            }
+        },
+        createLi: function (object) {
+            let li = this.createEle('li', this.ol, '')
+            let svg = this.createEle('svg', li, 'icon')
+            let use = this.createEle('use', svg, '')
+            use.setAttribute('xlink:href', "#icon-username")
+            let a = this.createEle('a', li, 'username')
+            a.textContent = object.attributes.username
+            let p = this.createEle('p', li, 'leaveMessageContent')
+            p.textContent = object.attributes.content
+            let span = this.createEle('span', li, 'time')
+            this.writeTime(object, span)
+        },
+        createEle: function (ele, parent, klassName) {
+            let element = document.createElement(ele)
+            element.className = klassName
+            parent.appendChild(element)
+            return element
+        },
+        writeTime: function writeTime(object, span) {
+            let year = object.updatedAt.getFullYear()
+            let month = object.updatedAt.getMonth() + 1
+            let date = object.updatedAt.getDate()
+            let hourse = object.updatedAt.getHours()
+            let minutes = object.updatedAt.getMinutes()
+            let seconds = object.updatedAt.getSeconds()
+            span.textContent = year + '年' + month + '月' + date + '日 ' + hourse + ':' + minutes + ':' + seconds
+        },
     }
 
-    function createLi(data) {
-        let ol = document.querySelector('#messageList')
-        let li = document.createElement('li')
-        ol.appendChild(li)
-        let svg = document.createElement('svg')
-        svg.className = 'icon'
-
-        let use = document.createElement('use')
-        use.setAttribute('xlink:href', "#icon-username")
-        svg.appendChild(use)
-        li.appendChild(svg)
-        let a = document.createElement('a')
-        a.textContent = data.attributes.username
-        a.className = 'username'
-        li.appendChild(a)
-        let p = document.createElement('p')
-        p.textContent = data.attributes.content
-        p.className = 'leaveMessageContent'
-        li.appendChild(p)
-        let span = document.createElement('span')
-        let year = data.updatedAt.getFullYear()
-        let month = data.updatedAt.getMonth() + 1
-        let date = data.updatedAt.getDate()
-        let hourse = data.updatedAt.getHours()
-        let minutes = data.updatedAt.getMinutes()
-        let seconds = data.updatedAt.getSeconds()
-        span.textContent = year + '年' + month + '月' + date + '日 ' + hourse + ':' + minutes + ':' + seconds
-        span.className = 'time'
-        li.appendChild(span)
-    }
-
-    let username = document.querySelector('#leaveMessageUsername')
-    let content = document.querySelector('#leaveMessageContent')
-    let submit = document.querySelector('#leaveMessageSubmit')
-    content.addEventListener('focus', (event) => {
-        event.currentTarget.classList.add('active')
-        let value = content.value
-        if (value === '') {
-            submit.classList.add('deactive')
-        } else {
-            submit.classList.add('active')
-        }
-    })
-    content.addEventListener('input', (event) => {
-        let value = content.value
-        if (value !== '') {
-            submit.classList.remove('deactive')
-            submit.classList.add('active')
-        } else {
-            submit.classList.remove('active')
-            submit.classList.add('deactive')
-        }
-    })
-    content.addEventListener('blur', (event) => {
-        let value = content.value
-        if (value !== '') {
-            submit.addEventListener('click', () => {
-                submitEvent()
-                content.classList.remove('active')
-                submit.classList.remove('active', 'deactive')
-            })
-        } else {
-            event.currentTarget.classList.remove('active')
-            submit.classList.remove('active', 'deactive')
-        }
-    })
+    controller.init(view)
 
 }.call()
